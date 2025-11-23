ec927faa16778168ac269ad44a97e139320da09a
Tommi <tommi.karsi@gmail.com>
2025-11-24 01:56:35 +0200

    streamlitapp muokkaus

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,30 +1,46 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-@st.cache_resource
+
+# Cachettaa tietokantayhteyden ja datan
+@st.cache_data(ttl=600)
 def mySql():
-    # Initialize connection.
+    # Initialize connection using Streamlit secrets
     conn = st.connection('mysql', type='sql')
-    # Perform query.
-    df_kyykky = conn.query('SELECT viikko, nimi, toistot, paino from kyykky LIMIT 100;', ttl=600)
-    df_penkki = conn.query('SELECT viikko, nimi, toistot, paino from penkki LIMIT 100;', ttl=600)
+    
+    # Hae data kyykky-taulusta
+    df_kyykky = conn.query('SELECT viikko, paino FROM kyykky LIMIT 100;')
     df_kyykky['harjoituslaji'] = 'kyykky'
+    
+    # Hae data penkki-taulusta
+    df_penkki = conn.query('SELECT viikko, paino FROM penkki LIMIT 100;')
     df_penkki['harjoituslaji'] = 'penkki'
     
     return df_kyykky, df_penkki
-    # Streamlit
+
+# Streamlit-sovellus
 def main():
-    st.title("dataa mysql tietokannasta")
-    st.write("kyykky ja penkki kehitys viikottain")
+    st.title("Kyykky ja Penkki kehitys viikottain")
+    st.write("Näytetään painot kehityksestä viikoittain")
+    
     df_kyykky, df_penkki = mySql()
-    #plot data
+    
     if not df_kyykky.empty or not df_penkki.empty:
+        # Yhdistä data
         df_combined = pd.concat([df_kyykky, df_penkki], ignore_index=True)
-        fig = px.line(df_combined, x="viikko", y="paino", color="nimi",
-                      line_dash="harjoituslaji",
-                      title='harjoitus painot viikottain', markers=True)
+        
+        # Piirrä viikkokohtainen painokehitys
+        fig = px.line(
+            df_combined, 
+            x="viikko", 
+            y="paino", 
+            color="harjoituslaji",  # eri värit kyykylle ja penkille
+            title="Harjoitus painot viikottain",
+            markers=True
+        )
         st.plotly_chart(fig, use_container_width=True)
     else:
         st.write("Ei dataa näytettäväksi")
+
 if __name__ == "__main__":
     main()
