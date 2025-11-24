23b895e6ed2fa22add0f92046c67830491c4da08
Tommi <tommi.karsi@gmail.com>
2025-11-24 05:11:37 +0200

    streamlitappin päivitys

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,46 +1,11 @@
 import streamlit as st
+import mysql.connector, secrets
 import pandas as pd
-import plotly.express as px
-
-# Cachettaa tietokantayhteyden ja datan
-@st.cache_data
-def mySql():
-    # Initialize connection using Streamlit secrets
-    conn = st.connection('mysql', type='sql')
-    
-    # Hae data kyykky-taulusta
-    df_kyykky = conn.query('SELECT viikko, paino FROM kyykky LIMIT 100;')
-    df_kyykky['harjoituslaji'] = 'kyykky'
-    
-    # Hae data penkki-taulusta
-    df_penkki = conn.query('SELECT viikko, paino FROM penkki LIMIT 100;')
-    df_penkki['harjoituslaji'] = 'penkki'
-    
-    return df_kyykky, df_penkki
-
-# Streamlit-sovellus
-def main():
-    st.title("Kyykky ja Penkki kehitys viikottain")
-    st.write("Näytetään painot kehityksestä viikoittain")
-    
-    df_kyykky, df_penkki = mySql()
-    
-    if not df_kyykky.empty or not df_penkki.empty:
-        # Yhdistä data
-        df_combined = pd.concat([df_kyykky, df_penkki], ignore_index=True)
-        
-        # Piirrä viikkokohtainen painokehitys
-        fig = px.line(
-            df_combined, 
-            x="viikko", 
-            y="paino", 
-            color="harjoituslaji",  # eri värit kyykylle ja penkille
-            title="Harjoitus painot viikottain",
-            markers=True
-        )
-        st.plotly_chart(fig, use_container_width=True)
-    else:
-        st.write("Ei dataa näytettäväksi")
-
-if __name__ == "__main__":
-    main()
+conn = mysql.connector.connect(host=secrets.host, user=secrets.user,
+password=secrets.password, database=secrets.database)
+df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50',
+conn)
+conn.close()
+st.title('Säädata Helsingistä')
+st.dataframe(df)
+st.line_chart(df.set_index('timestamp')['temperature'])
