f9aeee140ed9b55c9dfd43a8446140bb3f714b31
Tommi <tommi.karsi@gmail.com>
2025-11-24 02:49:13 +0200

    test

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector, secrets
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     # Connect to MySQL/MariaDB
     conn = mysql.connector.connect(
-    host=secrets.host,
-    user=secrets.user,
-    password=secrets.password,
-    database=secrets.database
+        host=secrets.host,
+        user=secrets.user,
+        password=secrets.password,
+        database=secrets.database
     )
     cursor = conn.cursor()
     cursor.execute("SELECT 'Hello from MySQL!'")
     result = cursor.fetchone()
-    # Clean up
     cursor.close()
     conn.close()
-    return f"<h1>{result[0]}</h1>"
+
+    greeting = result[0] if result else "Hello!"
+    return render_template("home.html", greeting=greeting)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
