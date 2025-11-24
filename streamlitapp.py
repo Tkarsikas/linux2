6f3f559cdc018caeaed9e4f5af94a5af5d6e6a4e
Tkarsikas <tommi.karsi@gmail.com>
2025-11-24 15:54:21 +0200

    limit poistettu

--- streamlitapp.py
+++ streamlitapp.py
@@ -9,7 +9,7 @@ conn = mysql.connector.connect(
     password=secrets.password,
     database=secrets.database
 )
-df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 1000', conn)
+df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp desc', conn)
 conn.close()
 st.title('Säädata Helsingistä')
 st.dataframe(df)
