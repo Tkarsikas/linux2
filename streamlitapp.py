c40f7149d977c849d8692bf7655dbb89d65c02e3
Tommi <tommi.karsi@gmail.com>
2025-11-24 05:13:16 +0200

    test

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import mysql.connector, secrets
 import pandas as pd
-conn = mysql.connector.connect(host=secrets.host, user=secrets.user,
-password=secrets.password, database=secrets.database)
+    conn = mysql.connector.connect(
+        host=secrets.host,
+        user=secrets.user,
+        password=secrets.password,
+        database=secrets.database
+    )
 df = pd.read_sql('SELECT * FROM weather_data ORDER BY timestamp DESC LIMIT 50',
 conn)
 conn.close()
