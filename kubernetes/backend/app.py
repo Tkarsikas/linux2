ba8ef54fdc371006aaf547db54272e75b7984223
Tkarsikas <tommi.karsi@gmail.com>
2025-12-08 15:53:16 +0200

    - merkin vaihto _ merkkiin

--- kubernetes/backend/app.py
+++ kubernetes/backend/app.py
@@ -54,7 +54,7 @@ def init_db():
         return jsonify({"error": str(e)}), 500
 
 @app.route('/api/add-user', methods=['POST'])
-def add-user():
+def add_user():
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
