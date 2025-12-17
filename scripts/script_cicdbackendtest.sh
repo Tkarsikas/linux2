055ab27ebfea334ae1e26da77dcd776e39b0eb4d
Tkarsikas <tommi.karsi@gmail.com>
2025-12-17 11:37:39 +0200

    backend test first init

--- scripts/script_cicdbackendtest.sh
+++ scripts/script_cicdbackendtest.sh
@@ -1,1 +1,17 @@
-URL="
+#!/bin/bash
+#test_backend.sh
+
+URL="http://localhost/cicd/api/health"
+
+echo "Testataan backend /api/health..."
+
+response=$(curl -s $URL)
+
+if [[ $response == *"ok"* ]]; then
+  echo "Backend is healthy ✅"
+  exit 0
+else
+  echo "Backend test FAILED ❌"
+  echo "Response: $response"
+  exit 1
+fi
